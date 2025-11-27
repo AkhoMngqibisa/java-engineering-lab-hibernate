9908d49f290a0a1955461c7ea45118bc46c16db5
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-27 19:15:00 +0200

    feat(hibernate): create a hibernate xml file for configs

--- src/main/java/com/akhona/Main.java
+++ src/main/java/com/akhona/Main.java
@@ -1,5 +1,10 @@
 package com.akhona;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,6 +12,13 @@ public class Main {
         student.setRollNo(123);
         student.setsName("Akhona");
         student.setsAge(22);
+
+        Configuration configuration = new Configuration();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        session.persist(student);
+
 
         System.out.println(student);
 
