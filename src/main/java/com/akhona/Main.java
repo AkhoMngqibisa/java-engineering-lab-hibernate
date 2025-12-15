2d1655af89c41e9e0f4d9f11328bb2168abcf759
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-12-15 19:45:00 +0200

    feat(hibernate): add a method that build a developer

--- src/main/java/com/akhona/Main.java
+++ src/main/java/com/akhona/Main.java
@@ -1,7 +1,23 @@
 package com.akhona;
 
+import com.akhona.entity.*;
+
 public class Main {
 
+    public Developer buildDeveloper() {
+        Laptop laptop = new Laptop();
+        laptop.setBrand("Lenovo");
+        laptop.setModel("i7");
+        laptop.setRam(16);
+
+        Developer developer = new Developer();
+        developer.setFirstName("Akhona");
+        developer.setLastName("Mngqibisa");
+        developer.setTechnology("Java");
+        developer.setLaptop(laptop);
+
+        return developer;
+    }
 
     public static void main(String[] args) {
 
